3644eec27f91cb44e8e74f864a276a8246b748b3
leandroyako <leandroyako@gmail.com>
2020-07-31 03:23:51 -0300

    blink svg

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const request = require('supertest');
 
 const LocalStorage = require('node-localstorage').LocalStorage;
 localStorage = new LocalStorage('./localStorage');
@@ -8,7 +7,7 @@ const composerRoutes = require('./routes/composerRoutes');
 const interpreterRoutes = require('./routes/interpreterRoutes');
 // express app
 const app = express();
-app.listen(3000);
+// app.listen(3000);
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -60,27 +59,57 @@ const options = {
 
 const osc = new OSC({ plugin: new OSC.DatagramPlugin(options) })
 
-osc.open() // start a WebSocket server on port 8080
+osc.open() 
 
-osc.on('/param/density', (message, rinfo) => {
-  console.log(message.args)
-  console.log(rinfo)
-})
+// osc.on('/param/density', (message, rinfo) => {
+//   console.log(message.args)
+//   console.log(rinfo)
+// })
 
-osc.on('*', message => {
-  console.log(message.args)
-})
+// osc.on('*', message => {
+//   console.log(message.args)
+// })
 
-osc.on('/{foo,bar}/*/param', message => {
-  console.log(message.args)
-})
+// osc.on('/{foo,bar}/*/param', message => {
+//   console.log(message.args)
+// })
 
 osc.on('open', () => {
   const message = new OSC.Message('/status', 'connected')
   osc.send(message)
 })
 
+
+// Ableton Link
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function(client){
+    client.on('event', function(data){});
+    client.on('disconnect', function(){});
+});
+
+const abletonlink = require('abletonlink');
+const link = new abletonlink();
+
+(() => {
+    let lastBeat = 0.0;
+    link.startUpdate(60, (beat, phase, bpm) => {
+        beat = 0 ^ beat;
+        if(0 < beat - lastBeat) {
+            io.emit('beat', { beat });
+            lastBeat = beat;
+        }
+    });
+})();
+
+server.listen(3000, () => {
+  console.log("**** listen on localhost:3000 ****");
+  console.log("access to http://localhost:3000/ !!");
+});
+
 /// test
+const request = require('supertest');
 
 request(app)
   .get('/composer/add/Tuba Demo')
