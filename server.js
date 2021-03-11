1a9290465de0da67e3f67db8d04643ffb4451938
leandroyako <leandroyako@gmail.com>
2021-03-11 13:11:41 -0300

    fix server host

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 // export constructor function that must be called to initialize this module
 module.exports = (app) => {
+    //const host = 'localhost'
+    const host = '0.0.0.0'
+    const port = 3000 || process.env.port
+
     const httpServer = require("http")
         .createServer(app)
-        .listen(3000 || process.env.port, "127.0.0.1", () => {
+        .listen(port, host, () => {
             const host = httpServer.address().address;
             const port = httpServer.address().port;
             console.log(`App listening at http://${host}:${port}`);
